db74bf44b0045cfe03fc6d4c4d05afcd39414438
Romain Monier <rmonier@insat.insa-toulouse.fr>
2021-10-18 15:13:45 +0200

    Added beginning of I2C driver (WIP)

--- Include/Service_IMU.h
+++ Include/Service_IMU.h
@@ -3,6 +3,10 @@
 
 #include "lsm9ds1_reg.h"
 
+/** Please note that is MANDATORY: return 0 -> no Error.**/
+int32_t platform_write(void *handle, uint8_t Reg, const uint8_t *Bufp, uint16_t len);
+int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp, uint16_t len);
+
 
 
 #endif
--- Source/Service_IMU.c
+++ Source/Service_IMU.c
@@ -1,2 +1,3 @@
 #include "Service_IMU.h"
 
+#include "Driver_I2C.h"
